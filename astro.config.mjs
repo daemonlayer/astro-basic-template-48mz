571deea7243974df05dc63902f34e2139180ea11
ruudmens <ruud.mens@gmail.com>
2025-12-05 20:58:20 +0100

    fix csp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
@@ -10,6 +9,36 @@ export default defineConfig({
     site: "https://daemonlayer.com",
     integrations: [tailwind(), mdx(), icon(), sitemap()],
     experimental: {
-        csp: true
+        csp: {
+            mode: "nonce", // Use nonces for inline scripts/styles
+            directives: {
+                "default-src": ["'self'"],
+                "script-src": [
+                    "'self'",
+                    "nonce", // allow Astro to inject nonces
+                    "https://sibforms.com",
+                    "https://api.mapbox.com"
+                ],
+                "style-src": [
+                    "'self'",
+                    "nonce",
+                    "https://fonts.googleapis.com",
+                    "https://sibforms.com"
+                ],
+                "font-src": [
+                    "'self'",
+                    "https://fonts.gstatic.com"
+                ],
+                "img-src": [
+                    "'self'",
+                    "data:",
+                    "https://api.mapbox.com"
+                ],
+                "worker-src": [
+                    "'self'",
+                    "blob:"
+                ]
+            }
+        }
     }
 });
